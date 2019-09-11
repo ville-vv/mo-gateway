523cc94f6b4a1d4a681da730168e910b3949f52e
ville <songwei@mintechai.com>
2019-09-11 14:27:00 +0800

    delete:some

--- src/server/grpc/grpc_server.go
+++ src/server/grpc/grpc_server.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"mo-gateway/src/config"
-	"mo-gateway/src/server/grpc/health"
 	"mo-gateway/src/server/grpc/pb"
 	"net"
 	"reflect"
@@ -27,13 +28,17 @@ func (sel *GrpcServer) Name() string {
 	return fmt.Sprintf("%s with %s", sel.name, sel.addr)
 }
 
+func (sel *GrpcServer) healthCheckServer(s *grpc.Server) {
+	hsrv := health.NewServer()
+	hsrv.SetServingStatus("mo-gateway", healthpb.HealthCheckResponse_SERVING)
+	healthpb.RegisterHealthServer(s, hsrv)
+}
+
 func (sel *GrpcServer) Start(...interface{}) error {
 	// 注入
 	s := grpc.NewServer()
 	pb.RegisterMoGatewayServer(s, sel)
-
-	health.New(s, "mo-gateway")
-
+	sel.healthCheckServer(s)
 	lis, err := net.Listen("tcp", sel.addr)
 	if err != nil {
 		return err
